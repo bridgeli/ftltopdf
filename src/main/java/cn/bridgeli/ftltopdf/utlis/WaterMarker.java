f222c92eb481194f87bcd108732f231e5781379b
BridgeLi <bridgeli.dev@gmail.com>
2022-12-06 22:41:32 +0800

    增加支持汉字水印

--- src/main/java/cn/bridgeli/ftltopdf/utlis/WaterMarker.java
+++ src/main/java/cn/bridgeli/ftltopdf/utlis/WaterMarker.java
@@ -1,8 +1,11 @@
 package cn.bridgeli.ftltopdf.utlis;
 
+import com.itextpdf.io.font.PdfEncodings;
 import com.itextpdf.kernel.events.Event;
 import com.itextpdf.kernel.events.IEventHandler;
 import com.itextpdf.kernel.events.PdfDocumentEvent;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.geom.Rectangle;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfPage;
@@ -11,10 +14,16 @@ import com.itextpdf.layout.Canvas;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.properties.TextAlignment;
 import com.itextpdf.layout.properties.VerticalAlignment;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Itext7 实现水印
  */
+@Slf4j
 public class WaterMarker implements IEventHandler {
 
     @Override
@@ -26,11 +35,22 @@ public class WaterMarker implements IEventHandler {
         Rectangle pageSize = page.getPageSize();
         PdfCanvas pdfCanvas = new PdfCanvas(page.getLastContentStream(), page.getResources(), pdf);
         Canvas canvas = new Canvas(pdfCanvas, pageSize);
-        Paragraph waterMarker = new Paragraph("JHPM").setOpacity(0.05f).setFontSize(40);
+        Paragraph waterMarker = new Paragraph("文字水印").setOpacity(0.05f).setFontSize(40);
+
+        InputStream inputStream = WaterMarker.class.getClassLoader().getResourceAsStream("STSONG.TTF");
+        PdfFont pdfFont = null;
+        if (null != inputStream) {
+            try {
+                byte[] bytes = IOUtils.toByteArray(inputStream);
+                pdfFont = PdfFontFactory.createFont(bytes, PdfEncodings.IDENTITY_H);
+            } catch (IOException e) {
+                log.error("加载字体报错", e);
+            }
+        }
 
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                canvas.showTextAligned(waterMarker, (150 + i * 300), (160 + j * 150), pdf.getNumberOfPages(), TextAlignment.CENTER, VerticalAlignment.BOTTOM, .6f);
+                canvas.setFontSize(100).setFont(pdfFont).showTextAligned(waterMarker, (150 + i * 300), (160 + j * 150), pdf.getNumberOfPages(), TextAlignment.CENTER, VerticalAlignment.BOTTOM, .6f);
             }
         }
         canvas.close();
